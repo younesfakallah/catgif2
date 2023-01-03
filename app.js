c8124def951fc32b825d2a02d63b39bf68d8a0d7
younesfakallah <senyoupl@icloud.com>
2023-01-03 17:44:23 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.sendFile(__dirname + '/index.html'));
+app.listen(3000, () => console.log("Server ready!"));
